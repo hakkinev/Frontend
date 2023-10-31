cbcead79f3f1fd188f42de2b18fd96a546501a7e
Vinski Häkkinen <vinski.hakkinen@gmail.com>
2023-10-31 11:58:35 +0200

    filerna för lektion 2

---
+++ script.js
@@ -0,0 +1,19 @@
+const namn = "JavaScript"
+const version = 1.0
+
+const str1 = "1. Hello " + namn + " version " + version + "!";
+
+function hello() {
+    const str2 = `Hello function!`;
+    console.log(str2);
+}
+
+function clickHandler () {
+    console.log("Du tryckte på knappen!")
+}
+
+hello();
+
+console.log(str1);
+
+
